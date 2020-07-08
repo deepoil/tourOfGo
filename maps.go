fa235ddf90a28dec307e97850839d5072b8a1c72
ryosuke <gava.1213@gmail.com>
2020-07-08 23:16:55 +0900

    first commit

---
+++ maps.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	wordCount := make(map[string]int)
+	//splitWord := strings.Split(s, " ")
+	splitWord := strings.Fields(s)
+	for _, char := range splitWord {
+		wordCount[char]++
+	}
+	return wordCount
+}
+
+func main() {
+	wc.Test(WordCount)
+}
+
